7f2b4640cc2fff00faf705bc988b338d6eb40373
shamaton <shamaton77@gmail.com>
2020-12-21 11:25:38 +0900

    developing

--- gen/gen.go
+++ gen/gen.go
@@ -67,7 +67,7 @@ func (g *generator) generate() {
 		Qual(pkTop, "SetResolver").Call(Id("encode"), Id("decode")),
 	)
 
-	decodeTopTemplate("decode", f).Block(
+	g.decodeTopTemplate("decode", f).Block(
 		If(Qual(pkTop, "StructAsArray").Call()).Block(
 			Return(Id("decodeAsArray").Call(Id("data"), Id("i"))),
 		).Else().Block(
@@ -75,18 +75,21 @@ func (g *generator) generate() {
 		),
 	)
 
-	decodeTopTemplate("decodeAsArray", f).Block(
+	g.decodeTopTemplate("decodeAsArray", f).Block(
 		Switch(Id("v").Op(":=").Id("i").Assert(Type())).Block(
-			cases()...,
+			g.decodeAsArrayCases()...,
 		),
 		Return(False(), Nil()),
 	)
 
-	decodeTopTemplate("decodeAsMap", f).Block(
+	g.decodeTopTemplate("decodeAsMap", f).Block(
+		Switch(Id("v").Op(":=").Id("i").Assert(Type())).Block(
+			g.decodeAsMapCases()...,
+		),
 		Return(False(), Nil()),
 	)
 
-	encodeTopTemplate("encode", f).Block(
+	g.encodeTopTemplate("encode", f).Block(
 		If(Qual(pkTop, "StructAsArray").Call()).Block(
 			Return(Id("encodeAsArray").Call(Id("i"))),
 		).Else().Block(
@@ -94,11 +97,17 @@ func (g *generator) generate() {
 		),
 	)
 
-	encodeTopTemplate("encodeAsArray", f).Block(
+	g.encodeTopTemplate("encodeAsArray", f).Block(
+		Switch(Id("v").Op(":=").Id("i").Assert(Type())).Block(
+			g.encodeAsArrayCases()...,
+		),
 		Return(Nil(), Nil()),
 	)
 
-	encodeTopTemplate("encodeAsMap", f).Block(
+	g.encodeTopTemplate("encodeAsMap", f).Block(
+		Switch(Id("v").Op(":=").Id("i").Assert(Type())).Block(
+			g.encodeAsMapCases()...,
+		),
 		Return(Nil(), Nil()),
 	)
 
@@ -109,20 +118,82 @@ func (g *generator) generate() {
 	fmt.Printf("%#v", f)
 }
 
-func decodeTopTemplate(name string, f *File) *Statement {
+func (g *generator) decodeTopTemplate(name string, f *File) *Statement {
 	return f.Func().Id(name).Params(Id("data").Index().Byte(), Id("i").Interface()).Params(Bool(), Error())
 }
 
-func encodeTopTemplate(name string, f *File) *Statement {
+func (g *generator) encodeTopTemplate(name string, f *File) *Statement {
 	return f.Func().Id(name).Params(Id("i").Interface()).Params(Index().Byte(), Error())
 }
 
-func cases() []Code {
+func (g *generator) encodeAsArrayCases() []Code {
 	var states []Code
 	for _, v := range analyzedStructs {
-		states = append(states, Case(Op("*").Qual(v.PackageName, v.Name)).Block(
-			List(Id("_"), Err()).Op(":=").Id(v.decodeArrayFuncName()).Call(Id("v"), Qual(pkDec, "NewDecoder").Call(Id("data")), Id("0")),
-			Return(True(), Err())))
+		for _, vv := range []string{"", "*"} {
+			states = append(states, Case(Op(vv).Qual(v.PackageName, v.Name)).Block(
+				Id(idEncoder).Op(":=").Qual(pkEnc, "NewEncoder").Call(),
+				List(Id("size"), Err()).Op(":=").Id(v.calcArraySizeFuncName()).Call(Id(vv+"v"), Id(idEncoder)),
+				If(Err().Op("!=").Nil()).Block(
+					Return(Nil(), Err()),
+				),
+				Id(idEncoder).Dot("MakeBytes").Call(Id("size")),
+				List(Id("b"), Id("offset"), Err()).Op(":=").Id(v.encodeArrayFuncName()).Call(Id(vv+"v"), Id(idEncoder), Lit(0)),
+				If(Err().Op("!=").Nil()).Block(
+					Return(Nil(), Err()),
+				),
+				If(Id("size").Op("!=").Id("offset")).Block(
+					Return(Nil(), Qual("fmt", "Errorf").Call(Lit(v.Name+" size / offset different %d : %d"), Id("size"), Id("offset"))),
+				),
+			))
+		}
+	}
+	return states
+}
+
+func (g *generator) encodeAsMapCases() []Code {
+	var states []Code
+	for _, v := range analyzedStructs {
+		for _, vv := range []string{"", "*"} {
+			states = append(states, Case(Op(vv).Qual(v.PackageName, v.Name)).Block(
+				Id(idEncoder).Op(":=").Qual(pkEnc, "NewEncoder").Call(),
+				List(Id("size"), Err()).Op(":=").Id(v.calcMapSizeFuncName()).Call(Id(vv+"v"), Id(idEncoder)),
+				If(Err().Op("!=").Nil()).Block(
+					Return(Nil(), Err()),
+				),
+				Id(idEncoder).Dot("MakeBytes").Call(Id("size")),
+				List(Id("b"), Id("offset"), Err()).Op(":=").Id(v.encodeMapFuncName()).Call(Id(vv+"v"), Id(idEncoder), Lit(0)),
+				If(Err().Op("!=").Nil()).Block(
+					Return(Nil(), Err()),
+				),
+				If(Id("size").Op("!=").Id("offset")).Block(
+					Return(Nil(), Qual("fmt", "Errorf").Call(Lit(v.Name+" size / offset different %d : %d"), Id("size"), Id("offset"))),
+				),
+			))
+		}
+	}
+	return states
+}
+
+func (g *generator) decodeAsArrayCases() []Code {
+	var states []Code
+	for _, v := range analyzedStructs {
+		for _, vv := range []string{"", "*"} {
+			states = append(states, Case(Op("*"+vv).Qual(v.PackageName, v.Name)).Block(
+				List(Id("_"), Err()).Op(":=").Id(v.decodeArrayFuncName()).Call(Id(vv+"v"), Qual(pkDec, "NewDecoder").Call(Id("data")), Id("0")),
+				Return(True(), Err())))
+		}
+	}
+	return states
+}
+
+func (g *generator) decodeAsMapCases() []Code {
+	var states []Code
+	for _, v := range analyzedStructs {
+		for _, vv := range []string{"", "*"} {
+			states = append(states, Case(Op("*"+vv).Qual(v.PackageName, v.Name)).Block(
+				List(Id("_"), Err()).Op(":=").Id(v.decodeMapFuncName()).Call(Id(vv+"v"), Qual(pkDec, "NewDecoder").Call(Id("data")), Id("0")),
+				Return(True(), Err())))
+		}
 	}
 	return states
 }
