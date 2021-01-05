7dafc74206638aaff2d29fcf19f6223e43856c86
shamaton <shamaton77@gmail.com>
2021-01-05 18:58:36 +0900

    developing

--- msgpack/enc/encoding.go
+++ msgpack/enc/encoding.go
@@ -2,7 +2,6 @@ package enc
 
 import (
 	"bytes"
-	"reflect"
 )
 
 type Encoder struct {
@@ -16,7 +15,6 @@ func NewEncoder() *Encoder {
 }
 
 func (e *Encoder) MakeBytes(size int) {
-	//e.d = make([]byte, size)
 	e.size = size
 	e.buf = bufPool.Get(size)
 	e.d = e.buf.Bytes()
@@ -28,131 +26,4 @@ func (e *Encoder) EncodedBytes() []byte {
 
 func (e *Encoder) ReleaseBytes() {
 	bufPool.Put(e.buf)
-}
-
-func (e *Encoder) create(rv reflect.Value, offset int) int {
-	return -1
-	/*
-		switch rv.Kind() {
-		case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint:
-			v := rv.Uint()
-			offset = e.WriteUint(v, offset)
-
-		case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
-			v := rv.Int()
-			offset = e.WriteInt(v, offset)
-
-		case reflect.Float32:
-			offset = e.WriteFloat32(rv.Float(), offset)
-
-		case reflect.Float64:
-			offset = e.WriteFloat64(rv.Float(), offset)
-
-		case reflect.Bool:
-			offset = e.WriteBool(rv.Bool(), offset)
-
-		case reflect.String:
-			offset = e.WriteString(rv.String(), offset)
-
-		case reflect.Slice:
-			if rv.IsNil() {
-				return e.WriteNil(offset)
-			}
-			l := rv.Len()
-			// bin format
-			if e.isByteSlice(rv) {
-				offset = e.writeByteSliceLength(l, offset)
-				offset = e.setBytes(rv.Bytes(), offset)
-				return offset
-			}
-
-			// format
-			offset = e.WriteSliceLength(l, offset)
-
-			if offset, find := e.writeFixedSlice(rv, offset); find {
-				return offset
-			}
-
-			// func
-			elem := rv.Type().Elem()
-			var f structWriteFunc
-			if elem.Kind() == reflect.Struct {
-				f = e.getStructWriter(elem)
-			} else {
-				f = e.create
-			}
-
-			// objects
-			for i := 0; i < l; i++ {
-				offset = f(rv.Index(i), offset)
-			}
-
-		case reflect.Array:
-			l := rv.Len()
-			// bin format
-			if e.isByteSlice(rv) {
-				offset = e.writeByteSliceLength(l, offset)
-				// objects
-				for i := 0; i < l; i++ {
-					offset = e.setByte1Uint64(rv.Index(i).Uint(), offset)
-				}
-				return offset
-			}
-
-			// format
-			offset = e.WriteSliceLength(l, offset)
-
-			// func
-			elem := rv.Type().Elem()
-			var f structWriteFunc
-			if elem.Kind() == reflect.Struct {
-				f = e.getStructWriter(elem)
-			} else {
-				f = e.create
-			}
-
-			// objects
-			for i := 0; i < l; i++ {
-				offset = f(rv.Index(i), offset)
-			}
-
-		case reflect.Map:
-			if rv.IsNil() {
-				return e.WriteNil(offset)
-			}
-
-			l := rv.Len()
-			offset = e.WriteMapLength(l, offset)
-
-			if offset, find := e.writeFixedMap(rv, offset); find {
-				return offset
-			}
-
-			// key-value
-			p := rv.Pointer()
-			for i := range e.mk[p] {
-				offset = e.create(e.mk[p][i], offset)
-				offset = e.create(e.mv[p][i], offset)
-			}
-
-		case reflect.Struct:
-			offset = e.writeStruct(rv, offset)
-
-		case reflect.Ptr:
-			if rv.IsNil() {
-				return e.WriteNil(offset)
-			}
-
-			offset = e.create(rv.Elem(), offset)
-
-		case reflect.Interface:
-			offset = e.create(rv.Elem(), offset)
-
-		case reflect.Invalid:
-			return e.WriteNil(offset)
-
-		}
-		return offset
-
-	*/
 }
