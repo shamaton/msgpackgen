3fefb4ee52f406759705fbebc0e91f240873a07d
shamaton <shamaton77@gmail.com>
2021-01-29 07:21:50 +0900

    developing

--- testdata/test.sh
+++ testdata/test.sh
@@ -5,10 +5,11 @@ CURRENT=$(cd "$DIR" && pwd)
 PJ=$(cd "$CURRENT" && cd ../ && pwd)
 cd "${PJ}" || exit 1
 
-find . -name "define*_test.go" -exec rename -v 's/_test//i' {} \;
+find . -name "define*_test.go" -maxdepth 1 | sed 's/_test.go//' | xargs -I{} mv {}_test.go {}.go;
 go generate
-find . -name "define*.go" -exec rename -v 's/\.go$/_test\.go/i' {} \;
+find . -name "define*.go" -maxdepth 1 | sed 's/.go//' | xargs -I{} mv {}.go {}_test.go;
 go test -v -count=1 \
   --coverpkg=github.com/shamaton/msgpackgen/... \
   --coverprofile=coverage.coverprofile \
   --covermode=atomic ./...
+git checkout resolver_test.go
